d5590b8dd6dc47e93882f8314030d328b2198cf1
Shayokh144 <shayokhbd144@gmail.com>
2023-04-28 16:13:51 +0700

    Widget Target Added

---
+++ LiveStockApp/StocksWidget/StocksWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  StocksWidgetBundle.swift
+//  StocksWidget
+//
+//  Created by nimble on 28/4/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StocksWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StocksWidget()
+        StocksWidgetLiveActivity()
+    }
+}
